f2cac29ead5da0382eb228e477cf3a8991bd24b1
ritapatacas <ritapatacas@users.noreply.github.com>
2025-03-10 04:07:53 +0000

    add fetching IPMA warnings

--- app/connections.py
+++ app/connections.py
@@ -10,10 +10,47 @@ MONGO_DB_NAME = "ipma"
 MONGO_COLLECTION_NAME = "ansiao"
 
 # ipma api request (ansião station)
-IPMA = {
+IPMA_STATION_OBSERVATIONS = {
     "ansiao_station": "1210716",
     "api_url": "https://api.ipma.pt/open-data/observation/meteorology/stations/observations.json"
 }
+
+IPMA_WARNINGS = {
+    "api_url": "https://api.ipma.pt/open-data/forecast/warnings/warnings_www.json"
+}
+
+IPMA_CLOSEST_REGIONS = [
+        {
+            "idRegiao": 1,
+            "idAreaAviso": "CBR",
+            "idConcelho": 3,
+            "globalIdLocal": 1060300,
+            "latitude": "40.2081",
+            "idDistrito": 6,
+            "local": "Coimbra",
+            "longitude": "-8.4194"
+        },
+        {
+            "idRegiao": 1,
+            "idAreaAviso": "LRA",
+            "idConcelho": 9,
+            "globalIdLocal": 1100900,
+            "latitude": "39.7473",
+            "idDistrito": 10,
+            "local": "Leiria",
+            "longitude": "-8.8069"
+        },
+        {
+            "idRegiao": 1,
+            "idAreaAviso": "CBO",
+            "idConcelho": 2,
+            "globalIdLocal": 1050200,
+            "latitude": "39.8217",
+            "idDistrito": 5,
+            "local": "Castelo Branco",
+            "longitude": "-7.4957"
+        }
+    ]
 
 def get_mongo_collection():
     MONGO_URI = os.getenv("MONGO_URI")
--- app/fetch.py
+++ app/fetch.py
@@ -1,19 +1,20 @@
 import requests
-from connections import get_mongo_collection, IPMA
+import json
+from collections import defaultdict
+from connections import get_mongo_collection, IPMA_STATION_OBSERVATIONS, IPMA_WARNINGS, IPMA_CLOSEST_REGIONS
 from utils import parse_datetime, logger
 
 collection = get_mongo_collection()
 
-
+# fetch observations data for ansriao station from IPMA API
 def fetch_stations_data():
     try:
-        res = requests.get(IPMA["api_url"])
+        res = requests.get(IPMA_STATION_OBSERVATIONS["api_url"])
         res.raise_for_status()
         return res.json()
     except requests.RequestException as e:
         logger.error(f"{e} error fetching")
         return None
-
 
 def fetch_and_store_ansiao_station():
     data = fetch_stations_data()
@@ -27,10 +28,10 @@ def fetch_and_store_ansiao_station():
                 "data_hora": parse_datetime(hour),
                 "data": parse_datetime(hour).strftime("%Y-%m-%d"),
                 "hora": parse_datetime(hour).strftime("%H:%M"),
-                **obs.get(IPMA["ansiao_station"], {})
+                **obs.get(IPMA_STATION_OBSERVATIONS["ansiao_station"], {})
             }
             for hour, obs in data.items()
-            if IPMA["ansiao_station"] in obs and obs[IPMA["ansiao_station"]] is not None
+            if IPMA_STATION_OBSERVATIONS["ansiao_station"] in obs and obs[IPMA_STATION_OBSERVATIONS["ansiao_station"]] is not None
         ]
     except Exception as e:
         logger.error(f"Error processing station data: {e}")
@@ -52,6 +53,71 @@ def fetch_and_store_ansiao_station():
         logger.error(f"Error storing data to MongoDB: {e}")
 
 
+#fetch weather warnings for closest regions from IPMA API
+# maybe we should store this information in the database (?) not sure yet...
+def fetch_warnings():
+    id_area_avisos = {region["idAreaAviso"] for region in IPMA_CLOSEST_REGIONS}
+
+    try:
+        res = requests.get(IPMA_WARNINGS["api_url"])
+        res.raise_for_status()
+        warnings = res.json()
+        
+        filtered_warnings = [
+            warning for warning in warnings
+            if warning.get("idAreaAviso") in id_area_avisos and warning.get("awarenessTypeName") != "Agitação Marítima"
+        ]
+        
+        organized_warnings = defaultdict(list)
+        for warning in filtered_warnings:
+            organized_warnings[warning["idAreaAviso"]].append({
+                "awarenessTypeName": warning.get("awarenessTypeName"),
+                "awarenessLevelID": warning.get("awarenessLevelID"),
+                "startTime": warning.get("startTime"),
+                "endTime": warning.get("endTime"),
+                "text": warning.get("text", "")
+            })
+
+        pretty_result = json.dumps(organized_warnings, indent=4, ensure_ascii=False)
+        # print(pretty_result)
+
+        return organized_warnings
+    except requests.RequestException as e:
+        logger.error(f"{e} error fetching warnings")
+        return None
+
+def aggregate_warnings_by_region(organized_warnings):
+    summary_dict = defaultdict(lambda: {"idsAreaAviso": []})
+
+    for area, warnings in organized_warnings.items():
+        for warning in warnings:
+            key = (
+                warning["awarenessTypeName"],
+                warning["awarenessLevelID"],
+                warning["startTime"],
+                warning["endTime"],
+                warning["text"]
+            )
+
+            if area not in summary_dict[key]["idsAreaAviso"]:
+                summary_dict[key]["idsAreaAviso"].append(area)
+
+            summary_dict[key].update({
+                "awarenessTypeName": warning["awarenessTypeName"],
+                "awarenessLevelID": warning["awarenessLevelID"],
+                "startTime": warning["startTime"],
+                "endTime": warning["endTime"],
+                "text": warning["text"]
+            })
+
+    aggregated_warnings = {"warnings": list(summary_dict.values())}
+
+    pretty_result = json.dumps(aggregated_warnings, indent=4, ensure_ascii=False)
+    print(pretty_result)
+
+    return aggregated_warnings
 
 if __name__ == "__main__":
     fetch_and_store_ansiao_station()
+    # fetch_warnings()
+    aggregate_warnings_by_region(fetch_warnings())
